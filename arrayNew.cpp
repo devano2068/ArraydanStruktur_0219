7f285985745f45140253b8dd11b77995cbc3a740
devano2068 <devano27921@gmail.com>
2026-04-06 17:48:02 +0700

    menampilkan

--- arrayNew.cpp
+++ arrayNew.cpp
@@ -1,14 +1,5 @@
 
 
-    // Menampilkan Satu per Satu
-    cout << "Data Pertama = " << kamu[0] << endl;
-    cout << "Data Kedua = " << kamu[1] << endl;
-    cout << "Data Ketiga = " << kamu[2] << endl;
-    cout << "Data Keempat = " << kamu[3] << endl;
-    cout << "Data Kelima = " << kamu[4] << endl;
-
- cout << endl;
-
     // Mengisi dengan menggunakan perulangan for
     for (int i = 0; i <= 4; i++)
     {
