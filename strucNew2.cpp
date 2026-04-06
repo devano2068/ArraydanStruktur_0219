0846e9a3f06939413332feb1ae0736eecea2d658
devano2068 <devano27921@gmail.com>
2026-04-06 19:28:20 +0700

    frist commit

--- strucNew2.cpp
+++ strucNew2.cpp
@@ -1,18 +1,4 @@
-#include <iostream>
-using namespace std;
 
-struct detailAlamat 
-{
-    string desa;
-    string kota;
-};
-
-struct orang 
-{
-    string nama;
-    detailAlamat alamat;
-    int umur;
-};
 
 int main() {
     orang mhs;
