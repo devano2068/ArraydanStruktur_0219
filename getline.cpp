d2b08ca2fd4f4b8ea28590b10795fb7d7b52f982
devano2068 <devano27921@gmail.com>
2026-04-06 18:46:14 +0700

    last commit

--- getline.cpp
+++
@@ -1,9 +0,0 @@
-
-
-    // menampilkan data
-    cout << "Menampilkan Data" << endl;
-    cout << "Nama : " << mhs.nama << endl;
-    cout << "Alamat : " << mhs.alamat.desa << ", " << mhs.alamat.kota << endl;
-    cout << "Umur : " << mhs.umur << endl;
- 
-}
