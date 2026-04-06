5890c5ab8428634f2524180297fa82234c0ee97b
devano2068 <devano27921@gmail.com>
2026-04-06 17:46:46 +0700

    deklarasi array

--- arrayNew.cpp
+++ arrayNew.cpp
@@ -1,12 +1,4 @@
-#include <iostream>
-using namespace std;
 
-int main() {
-    // deklarasi dengan inisialisasi
-    int aku[5] = {10,20,30,40,50};
-
-    // deklarasi array tanpa inisialisasi
-    int kamu[5];
 
     // tampilkan data (membaca data array)
     cout << "Data pada index 1 = " << aku[1] << endl;
