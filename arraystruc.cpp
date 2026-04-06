bbee08d27d4f75199cd8b0b95a00a421870d56ae
devano2068 <devano27921@gmail.com>
2026-04-06 18:59:52 +0700

    struc

--- arraystruc.cpp
+++ arraystruc.cpp
@@ -1,21 +1,4 @@
-#include <iostream>
-#include <string>
-using namespace std;
 
-// struct alamat
-struct detailAlamat 
-{
-    string desa;
-    string kota;
-};
-
-// struct orang
-struct orang 
-{
-    string nama;
-    detailAlamat alamat;
-    int umur;
-};
 
 int main() {
 
