70879c014b4f84c26c6ac7bb2d3265c23cb60851
devano2068 <devano27921@gmail.com>
2026-04-06 17:47:40 +0700

    menampilkan satu per satu

--- arrayNew.cpp
+++ arrayNew.cpp
@@ -1,23 +1,5 @@
 
 
-    // Mengisi Satu per Satu
-    cout << "Data index 0 = ";
-    cin >> kamu[0];
-
-    cout << "Data index 1 = ";
-    cin >> kamu[1];
-
-    cout << "Data index 2 = ";
-    cin >> kamu[2];
-
-    cout << "Data index 3 = ";
-    cin >> kamu[3];
-
-    cout << "Data index 4 = ";
-    cin >> kamu[4];
-
-    cout << endl;
-
     // Menampilkan Satu per Satu
     cout << "Data Pertama = " << kamu[0] << endl;
     cout << "Data Kedua = " << kamu[1] << endl;
